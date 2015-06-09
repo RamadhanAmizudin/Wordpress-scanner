67b1f788a8e544af89febd41dcde911a06f3fe28
Ramadhan Amizudin <ramadhan.amizudin@gmail.com>
2015-06-10 07:17:24 +0800

    add stub

--- build.php
+++ build.php
@@ -6,6 +6,9 @@ define('BIN_FOLDER', 'bin');
 $file_name = 'app.phar';
 
 $phar = new Phar( ROOT_PATH . DS . BIN_FOLDER . DS . $file_name, 0, 'app.phar' );
+// $phar = $phar->convertToExecutable(Phar::ZIP);
 $phar->buildFromDirectory( ROOT_PATH . DS);
-$phar->addFile( ROOT_PATH . DS .  'app.php');
-$phar->setDefaultStub('app.php');
+// $phar->addFile( ROOT_PATH . DS .  'app.php');
+$phar->setStub('<?php Phar::mapPhar();
+    include \'phar://\' . __FILE__ . \'/app.php\';
+    __HALT_COMPILER(); ?>');
