c306dcb2504a5ba08c5a1578330d08e9d58e8a6b
Ramadhan Amizudin <RamadhanAmizudin@users.noreply.github.com>
2015-06-11 13:06:16 +0800

    simplified

--- base/update.php
+++ base/update.php
@@ -1,15 +1,14 @@
 <?php
 function check_version() {
+	Config::set('nl', true);
 	$latest_version = 'https://raw.github.com/RamadhanAmizudin/Wordpress-scanner/master/app.php';
 	$src = HTTPRequest($latest_version);
 	preg_match("#define\('Version', '(.*?)'\);#i", $src, $o);
 	msg("[+] Current Version: ".Version);
 	if(isset($o[1])) {
 		if( version_compare(Version, $o[1], '<') ) {
-			Config::set('nl', true);
 			msg("[!] Newest version is available");
 		} else {
-			Config::set('nl', true);
 		  	msg("[!] No new version available");
 		}
 	}
